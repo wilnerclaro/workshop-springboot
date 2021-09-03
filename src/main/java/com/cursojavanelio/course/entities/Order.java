3a6a225b1bdd236d242e9cdb462949a462105430
wilnerclaro <wilnerclaro@gmail.com>
2021-09-03 17:10:37 -0300

    User entity and resource

---
+++ src/main/java/com/cursojavanelio/course/entities/Order.java
@@ -0,0 +1,14 @@
+package com.cursojavanelio.course.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Order implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private Date moment;
+	//private OrderStatus orderStatus;
+
+}
