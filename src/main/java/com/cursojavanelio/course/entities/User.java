c9c2e60d04fcb971e7bf005de6c34e1fbbad0d9d
wilnerclaro <wilnerclaro@gmail.com>
2021-09-03 17:31:16 -0300

    H2 database, test profile, JPA

--- src/main/java/com/cursojavanelio/course/entities/User.java
+++ src/main/java/com/cursojavanelio/course/entities/User.java
@@ -5,10 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -16,7 +24,7 @@ public class User implements Serializable{
 	private String password;
 	
 	
-	private List<Order> orders = new ArrayList<>();
+	//private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -70,13 +78,13 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-	public List<Order> getOrders() {
+	/*public List<Order> getOrders() {
 		return orders;
 	}
 
 	public void setOrders(List<Order> orders) {
 		this.orders = orders;
-	}
+	}*/
 
 	@Override
 	public int hashCode() {
