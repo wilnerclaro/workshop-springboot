47f6b0bc546a4a7a3e41ac538636ed5e2172e107
wilnerclaro <wilnerclaro@gmail.com>
2021-09-09 09:53:34 -0300

    User update

--- src/main/java/com/cursojavanelio/course/services/UserService.java
+++ src/main/java/com/cursojavanelio/course/services/UserService.java
@@ -31,5 +31,18 @@ public class UserService {
 	public void delete(Long id) {
 		userRepository.deleteById(id);
 	}
+	
+	public User update(Long id,User obj) {
+		User entity = userRepository.getById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+		
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 
 }
